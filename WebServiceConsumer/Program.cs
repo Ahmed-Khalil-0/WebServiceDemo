c48188b7598637e27af922371a7506a52ea893a7
AYA ibrahim <a.ibrahim@MCSFreeZone.onmicrosoft.com>
2021-07-26 11:24:44 +0200

    update

--- WebServiceConsumer/Program.cs
+++ WebServiceConsumer/Program.cs
@@ -8,8 +8,16 @@ namespace WebServiceConsumer
 {
     class Program
     {
+        //https://docs.microsoft.com/en-us/troubleshoot/dotnet/csharp/write-web-service
+
         static void Main(string[] args)
         {
+            ConsumeWebService();
+        }
+
+        static void ConsumeWebService()
+        {
+            //localhost.Service1 myMathService = new localhost.Service1();
             //Console.Write("2 + 4 = {0}", myMathService.Add(2,4));
         }
     }
